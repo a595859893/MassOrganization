04bec2ad635aaf639616dc74b1f68294583f8083
595859893 <595859893@qq.com>
2017-07-31 17:01:16 +0800

    后台数据更新，顺便修改了下command.js

--- CommonJS.js
+++ CommonJS.js
@@ -1,22 +1,22 @@
-	function GetXMLHTTPRequest(){
-		var xmlhttp;
-		if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
-			xmlhttp=new XMLHttpRequest();
-		}else{// code for IE6, IE5
-			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		return xmlhttp;
-	}
-	
-	function toStr(content){
-		var str = "";
-		var fir = true;
-		for(var n in content){
-			if(!fir)
-				str+="&";
-			else
-				fir=false;
-			str+=(n+"="+content[n]);
-		}
-		return str;
-	}
+function GetXMLHTTPRequest() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {// code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return xmlhttp;
+}
+
+function toStr(content) {
+    var str = "";
+    var fir = true;
+    for (var n in content) {
+        if (!fir)
+            str += "&";
+        else
+            fir = false;
+        str += (n + "=" + content[n]);
+    }
+    return str;
+}
--- actlistGet.php
+++ actlistGet.php
@@ -1,16 +1,25 @@
 <?php
 	header("Content-type: text/html; charset=utf-8");
 	if($_SERVER["REQUEST_METHOD"]=="POST"){
+		$place 	= _POST["place"];
+		$type 	= _POST["type"];
+		$order 	= _POST["order"];
+		
 		$mysqli = new mysqli('localhost','ipeuser','*ipE123','ipe_db','3306');
 		$line=array();
 		if($mysqli->connect_errno){
 			$line["error"] = "服务器连接失败，错误提示：".$mysqli->error;
 			$line["success"] = false;
 		}else{
-			switch($_POST["type"]){
-				case("new"):
-					$rst = $mysqli->query("SELECT * FROM activity as a WHERE 3>(SELECT count(*) FROM activity WHERE UID>a.UID) ORDER BY a.UID DESC");
-			}
+			$place = "'".preg_replace("/,/","','",$place)."'";
+			$type = "'".preg_replace("/,/","','",$type)."'";
+			$orderStr = "SELECT * FROM activity WHERE campus in(".$place.")AND type in(".$type.")";
+			if($order=="最新")
+				$orderStr.="ORDER BY time DESC";
+			else if($order=="最热")//有待完善
+				$orderStr.="ORDER BY time";
+			
+			$rst = $mysqli->query($orderStr);
 			
 			if($rst){
 				$looptag = 0;
