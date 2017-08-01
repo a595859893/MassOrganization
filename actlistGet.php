400282c145089e7e77f55c75f80d64eb9639ee73
595859893 <595859893@qq.com>
2017-08-01 15:21:42 +0800

    修复BUG，现在可以正常使用了，但是界面好像有点问题

--- actlistGet.php
+++ actlistGet.php
@@ -1,9 +1,9 @@
 <?php
 	header("Content-Type: text/html; charset=UTF-8");
 	if($_SERVER["REQUEST_METHOD"]=="POST"){
-		$place 	= _POST["place"];
-		$type 	= _POST["type"];
-		$order 	= _POST["order"];
+		$place 	= $_REQUEST["place"];
+		$type 	= $_REQUEST["type"];
+		$order 	= $_REQUEST["order"];
 		
 		$mysqli = new mysqli('localhost','ipeuser','*ipE123','ipe_db','3306');
 		$mysqli->query("set character set 'utf8'");
@@ -13,15 +13,13 @@
 			$line["error"] = "服务器连接失败，错误提示：".$mysqli->error;
 			$line["success"] = false;
 		}else{
-			$line["origin"] = _POST["place"]."WHY!!!!";
-			//$place = "'".preg_replace("/\,/","\'\,\'",$place)."','不限'";
-			//$type = "'".preg_replace("/\,/","\'\,\'",$type)."','不限'";
-			//$orderStr = "SELECT * FROM activity WHERE campus in(".$place.")AND type in(".$type.")";
+			$place = "'".preg_replace("/,/","','",$place)."','不限'";
+			$type = "'".preg_replace("/,/","','",$type)."','不限'";
+			$orderStr = "SELECT * FROM activity WHERE campus in(".$place.")AND type in(".$type.")";
 			if($order=="最新")
 				$orderStr.="ORDER BY time DESC";
 			else if($order=="最热")//有待完善
 				$orderStr.="ORDER BY time";
-			$orderStr = "SELECT * FROM activity";
 			
 			$rst = $mysqli->query($orderStr);
 			if($rst){
@@ -31,6 +29,7 @@
 					$looptag++;
 				}
 				$line["success"] = true;
+				$line["str"] = $orderStr;
 				$line["length"] = $looptag;
 			}else{
 				$line["error"] = "活动获取错误，错误提示: ".$mysqli->error;
