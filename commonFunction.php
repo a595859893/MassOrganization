481d84263aaabd0e680ca05ac46f75018cf1863a
595859893 <595859893@qq.com>
2017-08-29 22:58:51 +0800

    后端修复

--- commonFunction.php
+++ commonFunction.php
@@ -1,37 +1,32 @@
 <?php
-function linkToSQL(){
-	$mysqli = new mysqli('localhost','root','SYSUcc123','ipe_db','3306');
-	$mysqli->query("set character set 'utf8'");
-	$mysqli->query("set names 'utf8'");
-	if($mysqli->connect_errno){
-		$line["success"] = false;
-		$line["error"] = "连接服务器失败!".$mysqli->connect_errno;
-		$mysqli->close();
-		exit(json_encode($line));
-		return false;
-	}
-	return $mysqli;
+session_start();
+
+function linkToSQL()
+{
+    $mysqli = new mysqli('localhost', 'root', 'SYSUcc123', 'ipe_db', '3306');
+    $mysqli->query("set character set 'utf8'");
+    $mysqli->query("set names 'utf8'");
+    if ($mysqli->connect_errno) {
+        $line["success"] = false;
+        $line["error"] = "连接服务器失败!" . $mysqli->connect_errno;
+        $mysqli->close();
+        exit(json_encode($line));
+        return false;
+    }
+    return $mysqli;
 }
 
-function getOpenID(){
-	$openID = false;
-	if(isset($_COOKIE["openID"])){
-		$shaID = $_COOKIE["openID"];
-		$mysqli = linkToSQL();
-		$order = "SELECT openID from openID WHERE shaID=$shaID";
-		$rst = $mysqli->query($order);
-		while($row = $rst->fetch_array(MYSQLI_ASSOC)){
-			$openID = $row["openID"];
-			break;
-		}
-	}
-	return $openID;
+function getOpenID($mysqli)
+{
+    return  $_SESSION["openID"];;
 }
 
-function setError($code,$desc){
-	$err = array();
-	$err["code"] = $code;
-	$err["desc"] = $desc;
-	return $err;
+function setError($code, $desc)
+{
+    $err = array();
+    $err["code"] = $code;
+    $err["desc"] = $desc;
+    return $err;
 }
+
 ?>
--- conversationHot.php
+++ conversationHot.php
@@ -1,11 +1,11 @@
 <?php
-	require 'commonFunction.php';
-	$mysqli=linkToSQL();
-	$time = time();
-	$rst = $mysqli->query("SELECT * FROM conversation AS a WHERE a.UID NOT IN (SELECT conID FROM conversationHot) AND type='conversation' ORDER BY good DESC");
-	while($row = $rst->fetch_array(MYSQLI_ASSOC)){
-		$id = $row["UID"];
-		$rst = $mysqli->query("INSERT INTO conversationHot (conID,time) VALUES($id,FROM_UNIXTIME($time))");
-		break;
-	}
+require 'commonFunction.php';
+$mysqli = linkToSQL();
+$time = time();
+$rst = $mysqli->query("SELECT * FROM conversation AS a WHERE a.UID NOT IN (SELECT conID FROM conversationHot) AND type='conversation' ORDER BY good DESC");
+while ($row = $rst->fetch_array(MYSQLI_ASSOC)) {
+    $id = $row["UID"];
+    $rst = $mysqli->query("INSERT INTO conversationHot (conID,time) VALUES($id,FROM_UNIXTIME($time))");
+    break;
+}
 ?>
